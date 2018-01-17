09b0c909f0a7abc103e779356af7e5c4027d459c
mkhan <mkhan@allhomes.com.au>
2018-01-17 16:36:15 +1100

    up

--- src/testx.h
+++ src/testx.h
@@ -16,40 +16,41 @@
 #define MUFLIHUN_TEST_X_H
 
 #include <tuple>
+#include <type_traits>
 #include <vector>
 
 namespace muflihun {
 namespace testx {
 
-    template <typename... Args>
-    class TestCaseWrapper
-    {
-    public:    
-        template <std::size_t Index>
-        using TestCaseItemType = typename std::remove_reference<
-            decltype(std::get<Index>(std::tuple<Args...>()))
-                >::type;
-    
-        TestCaseWrapper(Args... args) : m_data(args...) {
-        }
-    
-        template <std::size_t Index>
-        auto get() const -> TestCaseItemType<Index> { 
-            return std::get<Index>(m_data); 
-        }
-    
-        const std::tuple<Args...>& data() const {
-            return m_data;
-        }
-    private:
-        std::tuple<Args...> m_data;
-    };
+template <typename... Args>
+class TestCaseWrapper
+{
+public:    
+    template <std::size_t Index>
+    using TestCaseItemType = typename std::remove_reference<
+        decltype(std::get<Index>(std::tuple<Args...>()))
+            >::type;
 
-    template <typename... T>
-    TestCaseWrapper<T...> TestCase(T... t) { return TestCaseWrapper<T...>(t...); }
+    TestCaseWrapper(Args... args) : m_data(args...) {
+    }
+    
+    template <std::size_t Index>
+    auto get() const -> TestCaseItemType<Index> { 
+        return std::get<Index>(m_data); 
+    }
+    
+    const std::tuple<Args...>& data() const {
+        return m_data;
+    }
+private:
+    std::tuple<Args...> m_data;
+};
 
-    template <typename... T>
-    using TestData = const std::vector<TestCaseWrapper<T...>>;
+template <typename... T>
+TestCaseWrapper<T...> TestCase(T... t) { return TestCaseWrapper<T...>(t...); }
+
+template <typename... T>
+using TestData = const std::vector<TestCaseWrapper<T...>>;
 
 } // namespace testx
 } // namespace muflihun
