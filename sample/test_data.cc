55bd4d58a61ed72ff54ccad9b8f4f08bc410983b
mkhan <mkhan@allhomes.com.au>
2018-01-17 16:49:54 +1100

    update

--- sample/test_data.cc
+++ sample/test_data.cc
@@ -1,18 +1,24 @@
 #include <cassert>
+#include <string>
 #include "../src/testx.h"
 
 using namespace muflihun::testx;
 
 int main() {
-	TestData<char, bool> IsNumberData = {
-		TestCase('1', true),
-		TestCase('a', false),
-		TestCase('a', true), // this will fail
-	};
-	
+    TestData<char, bool> IsNumberData = {
+        TestCase('1', true),
+        TestCase('a', false),
+        //TestCase('a', true), // this will fail - uncomment to witness
+    };
+    
     for (auto& testcase : IsNumberData) {
         char c = testcase.get<0>();
-		bool expected = testcase.get<1>();
-	    assert(isdigit(c) == expected);
+        bool expected = testcase.get<1>();
+        assert(isdigit(c) == expected);
     }
+
+    static TestData<std::string, bool> SecondIsNumberData = {
+        TestCase(std::string("1"), true), // either explicitly construct
+		TestCase<std::string, bool>("2", true), // or do this
+    };
 }
